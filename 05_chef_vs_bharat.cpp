2687f59f1539cf93c340f0d5c95303f5f0b76001
prashant singh <68744875+prashantsingh20@users.noreply.github.com>
2021-07-07 01:28:25 +0530

    Update 05_chef_vs_bharat.cpp

--- 05_chef_vs_bharat.cpp
+++ 05_chef_vs_bharat.cpp
@@ -1,76 +1,65 @@
+#include <iostream>
 #include <bits/stdc++.h>
-using namespace std;
 #define ll long long
+#define m 1000000007
+#define n 50000
+using namespace std;
 
-// getting no of digits
-ll countDigit(ll n) {
-    string s = to_string(n);
-    return s.length();
+ll modular_exponentiation(ll x,ll y){
+ x=x%m;
+ 
+ ll res=1;
+ 
+ while (y){
+  if(y&1)
+      res= ( (res%m)*(x%m) )%m;
+  x=( (x%m)*(x%m) )%m;
+  y=y>>1;
+ }
+
+ return res;
 }
 
-
-// power modulus
-ll power(ll x, ll y, ll p)
-{
-  ll res = 1;   
-  x = x % p;
-  if (x == 0) return 0;
-  while (y > 0) {
-    if (y & 1)
-      res = (res*x) % p;
-
-    y = y >> 1; 
-    x = (x*x) % p;
-  }
-  return res;
-}
-
-vector< pair <int,int>> findChefora(ll n) {
-    vector< pair <int,int> > vect;
-    ll arr[n];
-    arr[0] = -1;
-    ll pre_sum[n];
-    pre_sum[0] = 0;
-    vect.push_back(make_pair(arr[0],pre_sum[0]));
-    for(ll i=1; i < n+1; i++) {
-        if(i < 10)
-            arr[i] = i;
-        else {
-            ll length = countDigit(i);
-            string num1 = to_string(i);
-            string num2 = num1.substr(0,length -1);
-            reverse(num2.begin(), num2.end());
-            arr[i] = stoll(num1+num2);
+vector<ll> chefora(){
+    vector<ll>v(n);
+    v[0]=0;
+    for (ll i=1; i<=n;i++){
+        if (i>0 and i<10){
+            v[i]=i+v[i-1];
         }
-        pre_sum[i] = arr[i] + pre_sum[i-1];
-        //cout << arr[i] << " " << pre_sum[i] << endl;
-        
-        vect.push_back(make_pair(arr[i],pre_sum[i]));
+        else{
+           
+            ll num=i, rem=0;
+            num/=10;
+            int cnt=0;
+            ll j=i;
+             while(num>0){
+            rem=num%10;
+            cnt++;
+            num/=10;
+            j=j*10+rem;
+            
+             }
+             v[i]=j+v[i-1];
+        }
     }
-    return vect;
-    
+    return v;
 }
-
-
 
 int main() {
-    ll x = pow(10,5);
-    vector< pair <int,int> > vect = findChefora(x);
-
-    
-    ll t;
-    cin >> t;
-    
-    while(t--) {
-        int l,r;
-        cin >> l >> r;
-        
-        ll res = vect[r].second - vect[l].second;
-
-        ll p = pow(10,9) + 7;
-        ll ans = power(vect[l].first,res,p);
-        cout << ans << endl;
-     }
-  
-  return 0;
+ 
+ vector<ll>chef=chefora();
+ int t;
+ cin>>t;
+ 
+ while(t--){
+     ll l, r;
+     cin>>l>>r;
+     
+     ll power_sum=chef[r]-chef[l];
+     ll ans=modular_exponentiation(chef[l]-chef[l-1],power_sum);
+     cout<<ans<<endl;
+     
+ }
+ return 0;
 }
